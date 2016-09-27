96eec576040947adb0bc3a44c8af0d8adfe67664
xeyez <xeyez@naver.com>
2016-09-27 18:55:54 +0900

    fixed bugs

--- src/main/resources/springboard.sql
+++ src/main/resources/springboard.sql
@@ -7,10 +7,10 @@ SET SQL_SAFE_UPDATES = 0;
 create table tbl_board (
 	bno bigint not null auto_increment,
     title varchar(200) not null,
-    content text not null,
+    content longtext not null,
     writer varchar(50) not null,
-    regdate timestamp not null default now(),
-    updatedate timestamp not null,
+    regdate datetime not null default now(),
+    updatedate datetime NOT NULL default '1970-01-01 00:00:01',
     modcnt bigint not null default 0,
     viewcnt bigint not null default 0,
     goodcnt bigint not null default 0,
@@ -35,8 +35,8 @@ create table tbl_reply (
     bno bigint NOT NULL default 0,
     replytext varchar(1000) not null,
     replyer varchar(50) not null,
-    regdate timestamp not null default now(),
-    updatedate timestamp not null,
+    regdate datetime not null default now(),
+    updatedate datetime NOT NULL default '1970-01-01 00:00:01',
     modcnt bigint not null default 0,
     goodcnt bigint not null default 0,
     badcnt bigint not null default 0,
@@ -47,11 +47,35 @@ alter table tbl_reply add constraint fk_board
 foreign key (bno) references tbl_board (bno);
 
 
+#Insert sample
+INSERT INTO tbl_reply (BNO, REPLYTEXT, REPLYER)
+VALUES (1, 'test', 'admin');
+
+INSERT INTO tbl_reply (BNO, REPLYTEXT, REPLYER)
+VALUES (1, 'MANAGER!', 'manager');
+
+INSERT INTO tbl_reply (BNO, REPLYTEXT, REPLYER)
+VALUES (1, 'U S E R ★', 'user');
+
+insert into tbl_reply(bno, replytext, replyer)
+(select bno, replytext, replyer from tbl_reply);
+
+UPDATE tbl_board
+SET REPLYCNT =
+(
+	SELECT COUNT(RNO)
+	FROM tbl_reply
+	WHERE BNO = tbl_board.BNO
+)
+WHERE BNO > 0;
+
+
+
 
 create table tbl_attach (
 	fullName varchar(150) not null,
     bno bigint not null,
-    regdate timestamp default now(),
+    regdate datetime default now(),
     primary key(fullName)
 );
 
@@ -62,12 +86,12 @@ foreign key (bno) references tbl_board (bno);
 
 CREATE TABLE tbl_user (
 	userid VARCHAR(50) NOT NULL,
-    userpw VARCHAR(50) NOT NULL,
+    userpw longtext NOT NULL,
     username VARCHAR(100) NOT NULL,
     role varchar(50) NOT NULL default 'USER',
     upoint bigint NOT NULL DEFAULT 0,    
     profilepath varchar(100),
-    regdate timestamp default now(),    
+    regdate datetime default now(),    
     deactive tinyint NOT NULL default 0,
     deactivedate datetime NOT NULL default '1970-01-01 00:00:01',
     withdrawal tinyint NOT NULL default 0,    
